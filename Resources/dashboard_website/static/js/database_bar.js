2306c724ee246ff71cf58febda502d8cced2b06e
mvongjesda <marsha.vongjesda@gmail.com>
2020-04-10 12:56:46 -0400

    Restructure static folder

---
+++ Resources/dashboard_website/static/js/database_bar.js
@@ -0,0 +1,435 @@
+var data = [{
+    Movie_Title: "Snow White and the Seven Dwarfs",
+    Release_Date: "December 21, 1937",
+    Director: "David Hand",
+    Genre: "Musical",
+    MPAA_Rating: "G",
+    Total_Gross: 184925485,
+    Inflation_Adjusted_Gross: 5228953251
+  },
+  {
+    Movie_Title: "Pinocchio",
+    Release_Date: "February 7, 1940",
+    Director: "Ben Sharpsteen",
+    Genre: "Adventure",
+    MPAA_Rating: "G",
+    Total_Gross: 84300000,
+    Inflation_Adjusted_Gross: 2188229052
+  },
+  {
+    Movie_Title: "Fantasia",
+    Release_Date: "November 13, 1940",
+    Director: "full credits",
+    Genre: "Musical",
+    MPAA_Rating: "G",
+    Total_Gross: 8320000,
+    Inflation_Adjusted_Gross: 2187090808
+  },
+  {
+    Movie_Title: "Cinderella",
+    Release_Date: "February 15, 1950",
+    Director: "Wilfred Jackson",
+    Genre: "Drama",
+    MPAA_Rating: "G",
+    Total_Gross: 85000000,
+    Inflation_Adjusted_Gross: 920608730
+  },
+  {
+    Movie_Title: "Lady and the Tramp",
+    Release_Date: "June 22, 1955",
+    Director: "Hamilton Luske",
+    Genre: "Drama",
+    MPAA_Rating: "G",
+    Total_Gross: 93600000,
+    Inflation_Adjusted_Gross: 1236035515
+  },
+  {
+    Movie_Title: "Sleeping Beauty",
+    Release_Date: "January 29, 1959",
+    Director: "Clyde Geronimi",
+    Genre: "Drama",
+    MPAA_Rating: "Not Available",
+    Total_Gross: 9464608,
+    Inflation_Adjusted_Gross: 21505832 
+  },
+  {
+    Movie_Title: "The Sword in the Stone",
+    Release_Date: "December 25, 1963",
+    Director: "Wolfgang Reitherman",
+    Genre: "Adventure",
+    MPAA_Rating: "Not Available",
+    Total_Gross: 22182353,
+    Inflation_Adjusted_Gross: 153870834 
+  },
+  {
+    Movie_Title: "The Jungle Book",
+    Release_Date: "October 18, 1967",
+    Director: "Wolfgang Reitherman",
+    Genre: "Adventure",
+    MPAA_Rating: "Not Rated",
+    Total_Gross: 141843000,
+    Inflation_Adjusted_Gross: 789612346 
+  },
+  {
+    Movie_Title: "The Aristocats",
+    Release_Date: "December 24, 1970",
+    Director: "Wolfgang Reitherman",
+    Genre: "Musical",
+    MPAA_Rating: "G",
+    Total_Gross: 55675257,
+    Inflation_Adjusted_Gross: 255161499 
+  },
+  {
+    Movie_Title: "The Many Adventures of Winnie the Pooh",
+    Release_Date: "March 11, 1977",
+    Director: "Wolfgang Reitherman",
+    Genre: "Not Available",
+    MPAA_Rating: "Not Available",
+    Total_Gross: 0,
+    Inflation_Adjusted_Gross: 0 
+  },
+  {
+    Movie_Title: "The Rescuers",
+    Release_Date: "June 22, 1977",
+    Director: "Wolfgang Reitherman",
+    Genre: "Adventure",
+    MPAA_Rating: "Not Available",
+    Total_Gross: 48775599,
+    Inflation_Adjusted_Gross: 159743914 
+  },
+  {
+    Movie_Title: "The Fox and the Hound",
+    Release_Date: "July 10, 1981",
+    Director: "Art Stevens",
+    Genre: "Comedy",
+    MPAA_Rating: "Not Available",
+    Total_Gross: 43899231,
+    Inflation_Adjusted_Gross: 133118889 
+  },
+  {
+    Movie_Title: "The Black Cauldron",
+    Release_Date: "July 24, 1985",
+    Director: "Ted Berman",
+    Genre: "Adventure",
+    MPAA_Rating: "Not Available",
+    Total_Gross: 21288692,
+    Inflation_Adjusted_Gross: 50553142 
+  },
+  {
+    Movie_Title: "The Great Mouse Detective",
+    Release_Date: "July 2, 1986",
+    Director: "Ron Clements",
+    Genre: "Adventure",
+    MPAA_Rating: "Not Available",
+    Total_Gross: 23605534,
+    Inflation_Adjusted_Gross: 53637367
+  },
+  {
+    Movie_Title: "Oliver & Company",
+    Release_Date: "November 18, 1988",
+    Director: "George Scribner",
+    Genre: "Adventure",
+    MPAA_Rating: "G",
+    Total_Gross: 49576671,
+    Inflation_Adjusted_Gross: 102254492 
+  },
+  {
+    Movie_Title: "The Little Mermaid",
+    Release_Date: "November 17,1989 ",
+    Director: "Ron Clemens",
+    Genre: "Adventure",
+    MPAA_Rating: "G",
+    Total_Gross: 111543479,
+    Inflation_Adjusted_Gross: 223726012 
+  },
+  {
+    Movie_Title: "The Rescuers Down Under",
+    Release_Date: "November 16,1990",
+    Director: "Mike Gabriel",
+    Genre: "Adventure",
+    MPAA_Rating: "G",
+    Total_Gross: 27931461,
+    Inflation_Adjusted_Gross: 55796728
+  },
+  {
+    Movie_Title: "Beauty and the Beast",
+    Release_Date: "November 22,1991",
+    Director: "Gary Trousdale",
+    Genre: "Musical",
+    MPAA_Rating: "G",
+    Total_Gross: 218951625,
+    Inflation_Adjusted_Gross: 363017667
+  },
+  {
+    Movie_Title: "Aladdin",
+    Release_Date: "November 25,1992",
+    Director: "Ron Clements",
+    Genre: "Comedy",
+    MPAA_Rating: "G",
+    Total_Gross: 217350219,
+    Inflation_Adjusted_Gross: 441969178
+  },
+  {
+    Movie_Title: "The Lion King",
+    Release_Date: "June 24, 1994",
+    Director: "Roger Allers",
+    Genre: "Adventure",
+    MPAA_Rating: "G",
+    Total_Gross: 422780140,
+    Inflation_Adjusted_Gross: 761640898 
+  },
+  {
+    Movie_Title: "The Jungle Book",
+    Release_Date: "October 18,1967",
+    Director: "Wolfgang Reitherman",
+    Genre: "Adventure",
+    MPAA_Rating: "PG",
+    Total_Gross: 44342956,
+    Inflation_Adjusted_Gross: 88930321
+  },
+  {
+    Movie_Title: "Pocahontas",
+    Release_Date: "June 23,1995",
+    Director: "Mike Gabriel",
+    Genre: "Adventure",
+    MPAA_Rating: "G",
+    Total_Gross: 141579773,
+    Inflation_Adjusted_Gross: 274370957 
+  },
+  
+  {
+    Movie_Title: "The Jungle Book",
+    Release_Date: "October 18,1967",
+    Director: "Wolfgang Reitherman",
+    Genre: "Adventure",
+    MPAA_Rating: "PG",
+    Total_Gross: 44342956,
+    Inflation_Adjusted_Gross: 88930321 
+  },
+  {
+    Movie_Title: "The Hunchback of Notre Dame",
+    Release_Date: "June 21, 1996",
+    Director: "Gary Trousdale",
+    Genre: "Adventure",
+    MPAA_Rating: "G",
+    Total_Gross: 100138851,
+    Inflation_Adjusted_Gross: 190988799
+  },
+  {
+    Movie_Title: "Hercules",
+    Release_Date: "June 27, 1997",
+    Director: "Ron Clements",
+    Genre: "Adventure",
+    MPAA_Rating: "G",
+    Total_Gross: 99112101,
+    Inflation_Adjusted_Gross: 182029412
+  },
+  {
+    Movie_Title: "Mulan",
+    Release_Date: "June 19, 1998",
+    Director: "Barry Cook",
+    Genre: "Adventure",
+    MPAA_Rating: "G",
+    Total_Gross: 120620254,
+    Inflation_Adjusted_Gross: 216807832
+  },
+  {
+    Movie_Title: "Tarzan",
+    Release_Date: "June 18, 1999",
+    Director: "Chris Buck",
+    Genre: "Adventure",
+    MPAA_Rating: "G",
+    Total_Gross: 171091819,
+    Inflation_Adjusted_Gross: 283900254
+  },
+  {
+    Movie_Title: "Dinosaur",
+    Release_Date: "May 19, 2000",
+    Director: "Ralph Zondag",
+    Genre: "Adventure",
+    MPAA_Rating: "PG",
+    Total_Gross: 137748063,
+    Inflation_Adjusted_Gross: 215438985
+  },
+  {
+    Movie_Title: "The Emperor's New Groove",
+    Release_Date: "December 15, 2000",
+    Director: "Mark Dindal",
+    Genre: "Adventure",
+    MPAA_Rating: "G",
+    Total_Gross: 89296573,
+    Inflation_Adjusted_Gross: 136789252
+  }, 
+    {
+    Movie_Title: "Atlantis: The Lost Empire",
+    Release_Date: "June 15, 2001",
+    Director: "Gary Trousdale",
+    Genre: "Adventure",
+    MPAA_Rating: "PG",
+    Total_Gross: 84052762,
+    Inflation_Adjusted_Gross: 125188122
+  },   
+  {
+    Movie_Title: "Lilo & Stitch",
+    Release_Date: "June 21, 2002",
+    Director: "Chris Sanders",
+    Genre: "Adventure",
+    MPAA_Rating: "PG",
+    Total_Gross: 145771527,
+    Inflation_Adjusted_Gross: 211506702
+  }, 
+  {
+    Movie_Title: "Treasure Planet",
+    Release_Date: "November 27, 2002",
+    Director: "Ron Clements",
+    Genre: "Adventure",
+    MPAA_Rating: "PG",
+    Total_Gross: 38120554,
+    Inflation_Adjusted_Gross: 55189145
+  },  
+  {
+    Movie_Title: "Brother Bear",
+    Release_Date: "November 1, 2003",
+    Director: "Robert Walker",
+    Genre: "Adventure",
+    MPAA_Rating: "G",
+    Total_Gross: 85336277,
+    Inflation_Adjusted_Gross: 119218333
+  },  
+   {
+    Movie_Title: "Home on the Range",
+    Release_Date: "April 2, 2004",
+    Director: "Will Finn",
+    Genre: "Comedy",
+    MPAA_Rating: "PG",
+    Total_Gross: 50026353,
+    Inflation_Adjusted_Gross: 67910166
+  },   
+  {
+    Movie_Title: "Chicken Little",
+    Release_Date: "November 4, 2005",
+    Director:  "Mark Dindal",
+    Genre: "Adventure",
+    MPAA_Rating: "G",
+    Total_Gross: 135386665,
+    Inflation_Adjusted_Gross: 177954661
+  },  
+  {
+    Movie_Title: "Meet the Robinsons",
+    Release_Date: "March 30, 2007",
+    Director: "Stephen J. Anderson",
+    Genre: "Adventure",
+    MPAA_Rating: "G",
+    Total_Gross: 97822171,
+    Inflation_Adjusted_Gross:  119860589
+  },  
+  {
+    Movie_Title:  "Bolt",
+    Release_Date: "November 21, 2008",
+    Director: "Chris Williams",
+    Genre:  "Comedy",
+    MPAA_Rating: "PG",
+    Total_Gross: 114053759,
+    Inflation_Adjusted_Gross: 133702498
+  },   
+  {
+    Movie_Title: "The Princess and the Frog",
+    Release_Date:  "December 11, 2009",
+    Director: "Ron Clements",
+    Genre:  "Adventure",
+    MPAA_Rating: "G",
+    Total_Gross: 104400899,
+    Inflation_Adjusted_Gross: 116316457
+  },  
+    {
+    Movie_Title:  "Alice in Wonderland",
+    Release_Date: "July 28, 1951",
+    Director:  "Clyde Geronimi",
+    Genre: "Adventure",
+    MPAA_Rating: "PG",
+    Total_Gross: 334191110,
+    Inflation_Adjusted_Gross: 357063499
+  }, 
+    {
+    Movie_Title: "Tangled",
+    Release_Date: "November 24, 2010",
+    Director: "Nathan Greno",
+    Genre: "Adventure",
+    MPAA_Rating: "PG", 
+    Total_Gross: 200821936,
+    Inflation_Adjusted_Gross: 214388548
+  }, 
+   {
+    Movie_Title: "Winnie the Pooh",
+    Release_Date:   "July 15, 2011",
+    Director: "Stephen J. Anderson",
+    Genre: "Adventure",
+    MPAA_Rating: "G",
+    Total_Gross: 26692846,
+    Inflation_Adjusted_Gross: 28375869
+  },  
+  {
+    Movie_Title:  "Wreck-It Ralph",
+    Release_Date:  "November 2, 2012",
+    Director: "Rich Moore",
+    Genre: "Adventure",
+    MPAA_Rating: "PG",
+    Total_Gross: 189412677,
+    Inflation_Adjusted_Gross: 200354959
+  },  
+  {
+    Movie_Title: "Frozen",
+    Release_Date: "November 27, 2013",
+    Director: "Chris Buck",
+    Genre:  "Adventure",
+    MPAA_Rating: "PG",
+    Total_Gross: 400738009,
+    Inflation_Adjusted_Gross: 414997174
+  },  
+  {
+    Movie_Title: "Big Hero 6",
+    Release_Date: "November 7, 2014",
+    Director: "Don Hall",
+    Genre: "Adventure",
+    MPAA_Rating: "PG",
+    Total_Gross: 222527828,
+    Inflation_Adjusted_Gross: 229249222
+  },  
+  {
+    Movie_Title: "Cinderella",
+    Release_Date: "February 15, 1950",
+    Director: "Wilfred Jackson",
+    Genre: "Drama",
+    MPAA_Rating: "PG",
+    Total_Gross: 201151353,
+    Inflation_Adjusted_Gross: 201151353
+  },  
+  {
+    Movie_Title:  "Zootopia",
+    Release_Date: "March 4, 2016",
+    Director:  "Byron Howard",
+    Genre: "Adventure",
+    MPAA_Rating: "PG",
+    Total_Gross: 341268248,
+    Inflation_Adjusted_Gross: 341268248
+  },  
+   {
+    Movie_Title: "The Jungle Book",
+    Release_Date: "October 18, 1967",
+    Director: "Wolfgang Reitherman",
+    Genre:  "Adventure",
+    MPAA_Rating: "PG",
+    Total_Gross: 364001123,
+    Inflation_Adjusted_Gross: 364001123 
+  },
+  {
+    Movie_Title: "Moana",
+    Release_Date:  "November 23, 2016",
+    Director: "Ron Clements",
+    Genre: "Adventure",
+    MPAA_Rating: "PG",
+    Total_Gross: 246082029,
+    Inflation_Adjusted_Gross: 246082029
+  }
+];  
+    
